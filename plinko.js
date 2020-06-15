0270bcc90876667d2718f4b9d830f89414db7ad8
JayGupta05 <65399608+JayGupta05@users.noreply.github.com>
2020-06-15 11:28:34 +0530

    Add files via upload

---
+++ plinko.js
@@ -0,0 +1,16 @@
+class Plinko{
+    constructor(x,y){
+        var options = {
+            isStatic: true
+        }
+        this.body = Matter.Bodies.circle(x,y,10,options);
+        World.add(world,this.body);
+    }
+    display(){
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        ellipseMode(RADIUS);
+        ellipse(0,0,20,20);
+        pop();
+    }
+}
